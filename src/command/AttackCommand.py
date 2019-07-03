418d65602d90070185df6c6b766d76e3551b4e3a
tim <exedary@gmail.com>
2019-07-03 14:30:27 +0600

    implemented command arch

---
+++ src/command/AttackCommand.py
@@ -0,0 +1,5 @@
+from Command import Command
+class AttackCommand(Command) : 
+    def execute(self) :
+        #TODO implement method to attack connected with PlayerConsoleController
+        pass
---
+++ src/command/MoveCommand.py
@@ -0,0 +1,5 @@
+from Command import Command
+class MoveCommand(Command) : 
+    def execute(self) :
+        #TODO : implement PlayerConsoleController
+        pass
