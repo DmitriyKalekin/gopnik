14b004a53dc8f84dd973468d9b1363af9eae9c06
tim <exedary@gmail.com>
2019-07-03 22:57:08 +0600

    finished playerController

--- src/controllers/PlayerController.py
+++ src/controllers/PlayerController.py
@@ -1,6 +1,7 @@
-from MoveCommand import MoveCommand
-from AttackCommand import AttackCommand
+from command.MoveCommand import MoveCommand
+from command.AttackCommand import AttackCommand
 class PlayerController :
+    Settings = dict(Move = 'w', Attack = 'a', Exit = 'q')
     def __init__(self, attack : AttackCommand, move : MoveCommand) :
         self.attack = attack
         self.move = move
@@ -8,4 +9,10 @@ class PlayerController :
         self.attack.execute()
     def Move(self) :
         self.move.execute()
-
+    def update(self, key) : 
+        if key == self.Settings.get('Move') :
+            self.Attack()
+        if key == self.Settings.get('Attack') :
+            self.Move()
+        if key == self.Settings.get('Exit') :
+            exit()
