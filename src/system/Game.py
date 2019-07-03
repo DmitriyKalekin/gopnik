ac665ac69e65fcfbb0944e6e52180e6a5ad76fc1
jebbyk <zaicev9797@gmail.com>
2019-07-03 21:57:27 +0600

    Add Game class

---
+++ src/system/Game.py
@@ -0,0 +1,26 @@
+class Game:
+    is_runing = False
+    players = []
+
+    def __init__ (self, cfgFile = "/path/to/_file"):
+        pass
+
+    def Start():
+        #self.is_runing = True
+        pass
+    
+    def Update():
+        pass
+
+    def Pause():
+        #self.is_runing = False
+        pass
+
+    def Stop():
+        #self.is_runing = False
+        pass
+
+    def GameLoop(loop_time = 0.1):
+        while self.is_runing:
+            pass
+
