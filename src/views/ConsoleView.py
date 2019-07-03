70f887555a1be6afc28a3812bdbf7408c7144c89
jebbyk <zaicev9797@gmail.com>
2019-07-03 21:35:04 +0600

    Create ConsoleView class

---
+++ src/views/ConsoleView.py
@@ -0,0 +1,16 @@
+import View
+class ConsoleView ():
+    def __init__(self, console, colored = False, charset = "utf-8"):
+        pass
+
+    def get_info():
+        pass
+
+    def push_content():
+        pass
+
+    def clear_view_buffer():
+        pass
+
+    def listen_player_input():
+        pass
