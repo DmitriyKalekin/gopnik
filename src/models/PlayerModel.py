bb2ba2469402dbf964a517c4592aeee74e820c93
tim <exedary@gmail.com>
2019-07-04 16:18:42 +0600

    implemented players attack and move

--- src/models/PlayerModel.py
+++ src/models/PlayerModel.py
@@ -1,9 +1,22 @@
 from models.CreatureModel import CreatureModel
+from EnemyModel import EnemyModel
+import random
 class PlayerModel(CreatureModel) : 
     def __init__(self):
         pass
-    def Attack(self) :
-        #idk is it right place for player methods or not 
-        pass
+    def Attack(self, enemy : EnemyModel) :
+        ChanceToHit = random.randint(0,20)
+        if ChanceToHit < 7 : 
+            return 'unsuccessful'
+        if ChanceToHit > 7 :
+            return 'successful'
+        if ChanceToHit == 20 : 
+            return 'Critical'
     def Move(self) : 
-        pass
+        chanceToMeet = random.randint(0,1)
+        if chanceToMeet == 1 :
+            print("You've met someone")
+            return 'zavarushka'
+        if chanceToMeet == 0 :
+            print("You're still save")
+            return 'savezone'
